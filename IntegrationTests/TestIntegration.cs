d9ae2e4124623decc84f7394145f26be182b7c1c
pavel pesetskiy <pavel_pesetskiy@epam.com>
2016-04-28 15:02:07 +0300

    add integratio

---
+++ IntegrationTests/TestIntegration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+//using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    [TestFixture]
+    public class TestIntegration
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
